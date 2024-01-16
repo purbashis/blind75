f49a5b6c0cb84b54484344bb05bdcac8fda37914
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2024-01-16 23:13:50 +0530

    word_break-leetcode-139

---
+++ word_break.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        Set<String> wordSet = new HashSet<>(wordDict);
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
+//leetcode -139 
