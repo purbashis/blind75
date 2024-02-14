9911c7b109d19cef0648b734608b73b3e850033f
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2024-02-14 22:09:49 +0530

    Rearrange Array Elements by Sign- leetcode - 2149

---
+++ rearrange_array_elements_by_sign.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        var res = new int[nums.length];
+        var pos = 0;
+        var neg = 1;
+
+        for (var num : nums) {
+            if (num > 0) {
+                res[pos] = num;
+                pos += 2;
+            } else {
+                res[neg] = num;
+                neg += 2;
+            }
+        }
+        return res;
+    }
+}
+// 2149. Rearrange Array Elements by Sign
